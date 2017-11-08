016a74119863126bf351f6f40d6e0323b18e3b92
krisdianagus <32463039+krisdianagus@users.noreply.github.com>
2017-11-08 10:40:16 +0700

    no.5

---
+++ Untitled1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	float berat;
+	int tinggi;
+	
+	cout << "Masukkan tinggi badan dalam cm : ";
+	cin >> tinggi;
+	
+	cout << "Masukkan berat badan dalam kg : ";
+	cin >> berat;
+}
