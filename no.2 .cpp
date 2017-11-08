15477fa23bdf8415b81a8a630c1613ad82e46c82
krisdianagus <32463039+krisdianagus@users.noreply.github.com>
2017-11-08 11:32:20 +0700

    perbaikan .cpp

--- no.2 .cpp
+++ no.2 .cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-using namespase std;
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
 
 int main () {
 char bidang;
 double p,l,a,t,
 
-cout << "pilihan bidang : ";
+cout << "pilih bidang : ";
 cin >> bidang;
+
+
+if (bidang == 'p' || bidang == 'p') {
+	cout << " menghitung luas persegi : ";
+	cout << " masukkan panjang : ";
+	cin >> p;
+	cout << "Masukkan lebarnya : ";
+	cin >> l;
+	L = p*l;
+	cout << " Luas persegi panjang : ";
+	}
+	
+	else{
+		cout<<" Maaf. anda salah memencet pilihan"<<endl;
+	}
 }
