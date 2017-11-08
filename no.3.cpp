af91c001c812d0e95532c048df99b40e24d5f07a
krisdianagus <32463039+krisdianagus@users.noreply.github.com>
2017-11-08 11:44:04 +0700

    percobaan.cpp

--- no.3.cpp
+++ no.3.cpp
@@ -4,4 +4,15 @@
 using namespace std;
 
 int main (){
+	int ga,jl,lembur,gt,pajak;
+	
+	cout << "masukkan gaji Pegawai : ";
+	cin >> ga;
+	cout << "masukkan jam pegawai : ";
+	cin >> jl;
+	
+	lembur= jl - 30;
+ 	ga= (lembur*1,5=40)+gt;
+ 	pajak=gt*0.5;
+	gt=gt-pajak;
 }
